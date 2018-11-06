7fb7fc897b56bcbee6e6b08f261bba95659120b5
Larry Schirmer <larryschirmer@gmail.com>
2018-11-05 21:29:11 -0500

    add delete logic to schema and resolver

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,6 +3,7 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   item: forwardTo('db'),
   items: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
   // async items(parent, args, ctx, info) {
   //   const items = await ctx.db.query.items();
   //   return items;
